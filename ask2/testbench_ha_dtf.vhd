c91ebccec5a07ad6119108a68424c4bda0407e79
Konstantinos Agiannis <k.agiannis@yandex.com>
2020-03-25 21:54:42 +0200

    update

--- ask2/testbench_ha_dtf.vhd
+++ ask2/testbench_ha_dtf.vhd
@@ -56,18 +56,18 @@ stim_proc:process
 begin
 A<='0';
 B<='0';
-wait for 10ns;
+wait for 10 ns;
 A<='0';
 B<='1';
 
- wait for 10ns;
+ wait for 10 ns;
 A<='1';
 B<='0';
- wait for 10ns; 
+ wait for 10 ns; 
  
 A<='1';
 B<='1';
- wait for 10ns;
+ wait for 10 ns;
 
 end process;
 
