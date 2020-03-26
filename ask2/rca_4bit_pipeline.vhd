288fb457322bec87f537ab3fcabbe43e594b7ff2
Konstantinos Agiannis <k.agiannis@yandex.com>
2020-03-26 22:27:26 +0200

    fix

--- ask2/rca_4bit_pipeline.vhd
+++ ask2/rca_4bit_pipeline.vhd
@@ -31,10 +31,12 @@ signal b_regs : register_arr;
 
 
 signal c_regs : std_logic_vector(4 downto 0);
+signal c2_regs : std_logic_vector(4 downto 0);
+signal s2_regs : std_logic_vector(4 downto 0);
 
 begin
 f1:for k in 0 to 3 generate
-        u1:full_adder_bhv_seq port map(A=>a_regs(k)(k), B=>b_regs(k)(k), Cin=>c_regs(k), S=>a_regs(k+1)(k), Cout=>c_regs(k+1), clk=>clk);
+        u1:full_adder_bhv_seq port map(A=>a_regs(k)(k), B=>b_regs(k)(k), Cin=>c_regs(k), S=>s2_regs(k), Cout=>c2_regs(k+1), clk=>clk);
     end generate f1;
 process(clk)
 begin
@@ -47,18 +49,22 @@ if rising_edge(clk) then
     
     if rst = '1' then
         c_regs <= (others=>'0');
-        for i in 0 to 3 loop
+        for i in 0 to 4 loop
             a_regs(i) <= (others=>'0');
             b_regs(i) <= (others=>'0');
         end loop;
-    else  
+    else
+    for i in 1 to 4 loop
+        c_regs(i) <= c2_regs(i);
+        a_regs(i)(i-1) <= s2_regs(i-1);
+    end loop;
         for ii in 1 to 4 loop
             for j in 0 to 3 loop
                 if (ii/=(j+1)) then
                     a_regs(ii)(j) <= a_regs(ii-1)(j);
                     b_regs(ii)(j) <= b_regs(ii-1)(j); 
                 end if;
-            end loop;    
+            end loop;
         end loop; 
     end if;
 end if;
