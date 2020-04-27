5f4aa0917ff16548bbd84e9cce679c05e04d2e21
markiiZZ <48489117+markiiZZ@users.noreply.github.com>
2020-04-27 09:05:02 +0300

    Add files via upload

---
+++ ask3/zitoumeno2/types.vhd
@@ -0,0 +1,18 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+USE ieee.math_real.all;
+
+PACKAGE types IS
+
+	CONSTANT taps        : INTEGER := 8; --number of fir filter taps
+	CONSTANT data_width  : INTEGER := 8; --width of data input including sign bit
+	CONSTANT coeff_width : INTEGER := 8; --width of coefficients including sign bit
+	constant ZERO : UNSIGNED(10 downto 0) := (others => '0');
+	
+	TYPE coefficient_array IS ARRAY (0 TO taps-1) OF STD_LOGIC_VECTOR(coeff_width-1 DOWNTO 0);  --array of all coefficients
+	TYPE data_array IS ARRAY (0 TO taps-1) OF UNSIGNED(data_width-1 DOWNTO 0);                    --array of historic data values
+	TYPE product_array IS ARRAY (0 TO taps-1) OF UNSIGNED((data_width + coeff_width)-1 DOWNTO 0); --array of coefficient * data products
+	type ADD_TYPE is array(0 TO taps-1) of UNSIGNED(18 downto 0);
+	
+END PACKAGE types;
