551055f744249645296c4c04065073d71b4539bf
markiiZZ <48489117+markiiZZ@users.noreply.github.com>
2020-04-27 09:06:36 +0300

    Update types.vhd

--- ask3/zitoumeno2/types.vhd
+++ ask3/zitoumeno2/types.vhd
@@ -6,8 +6,8 @@ USE ieee.math_real.all;
 PACKAGE types IS
 
 	CONSTANT taps        : INTEGER := 8; --number of fir filter taps
-	CONSTANT data_width  : INTEGER := 8; --width of data input including sign bit
-	CONSTANT coeff_width : INTEGER := 8; --width of coefficients including sign bit
+	CONSTANT data_width  : INTEGER := 8; --width of data input 
+	CONSTANT coeff_width : INTEGER := 8; --width of coefficients 
 	constant ZERO : UNSIGNED(10 downto 0) := (others => '0');
 	
 	TYPE coefficient_array IS ARRAY (0 TO taps-1) OF STD_LOGIC_VECTOR(coeff_width-1 DOWNTO 0);  --array of all coefficients
