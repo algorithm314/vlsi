32eebb643fc618d6cb4677e59e5b56a4a6506285
markiiZZ <48489117+markiiZZ@users.noreply.github.com>
2020-04-29 14:28:19 +0300

    add registers after mult (valid in/out not yet )

--- ask3/fir_filter.vhd
+++ ask3/fir_filter.vhd
@@ -9,11 +9,11 @@ PACKAGE types IS
 	CONSTANT taps        : INTEGER := 8; --number of fir filter taps
 	CONSTANT data_width  : INTEGER := 8; --width of data input 
 	CONSTANT coeff_width : INTEGER := 8; --width of coefficients 
-	constant ZERO : UNSIGNED(18 downto 0) := (others => '0');
+	constant ZERO : UNSIGNED(data_width + coeff_width + integer(ceil(log2(real(taps)))) - 1  downto 0) := (others => '0');
 	
 	TYPE coefficient_array IS ARRAY (0 TO taps-1) OF STD_LOGIC_VECTOR(coeff_width-1 DOWNTO 0);  --array of all coefficients
-	CONSTANT COEFS : coefficient_array := ("00000001", "00000010", "00000011", "00000100", "00000101", "00000110", "00000111",
-                             "00001000"); 
+    CONSTANT COEFS : coefficient_array := ("00000001", "00000010", "00000011", "00000100", "00000101", "00000110", "00000111", "00001000");
+   -- CONSTANT COEFS : coefficient_array := ("00000001", "00000001", "00000001", "00000001","00000001"); 
 	TYPE data_array IS ARRAY (0 TO taps-1) OF UNSIGNED(data_width-1 DOWNTO 0);                    --array of historic data values
 	TYPE product_array IS ARRAY (0 TO taps-1) OF UNSIGNED((data_width + coeff_width)-1 DOWNTO 0); --array of coefficient * data products
 	type ADD_TYPE is array(0 TO taps-1) of UNSIGNED(data_width + coeff_width + integer(ceil(log2(real(taps)))) - 1 downto 0);
@@ -41,9 +41,13 @@ ENTITY fir_filter IS
 END fir_filter;
 
 ARCHITECTURE behavior OF fir_filter IS
-	SIGNAL data_temp : data_array;        --extra latency 
+	SIGNAL data_temp, data_temp2, data_temp3, data_temp4, data_temp5, data_temp6, data_temp7 : data_array;        --extra latency
+	SIGNAL products 		: product_array;     --array of coefficient*data products
+	attribute syn_multstyle : string;
+    attribute syn_multstyle of products : signal is "logic";
+	
 BEGIN
-
+    
 	PROCESS(clk, reset_n)
 		
 	BEGIN
@@ -52,23 +56,35 @@ BEGIN
 		
 			data_pipeline <= (OTHERS => (OTHERS => '0'));              --clear data pipeline values
 			data_temp <= (OTHERS => (OTHERS => '0'));
+--			data_temp2 <= (OTHERS => (OTHERS => '0'));
+--			data_temp3 <= (OTHERS => (OTHERS => '0'));
+--			data_temp4 <= (OTHERS => (OTHERS => '0'));
+--			data_temp5 <= (OTHERS => (OTHERS => '0'));
+--			data_temp6 <= (OTHERS => (OTHERS => '0'));
+--			data_temp7 <= (OTHERS => (OTHERS => '0'));
+			products <= (OTHERS => (OTHERS => '0'));
 		    ADD <= (OTHERS => (OTHERS => '0'));
 			result <= (OTHERS => '0');                                  --clear result output
 			
 		ELSIF(clk'EVENT AND clk = '1') THEN                          --not reset
 		
-			data_temp <= UNSIGNED(data) & data_pipeline(0 TO taps-2);	--shift new data into data pipeline
-            data_pipeline <= data_temp;
+		    result <= std_logic_vector(ADD(taps-1));	                           --output result
+--		    data_temp2 <= data_temp;
+--		    data_temp3 <= data_temp2;
+--		    data_temp4 <= data_temp3;
+--		    data_temp5 <= data_temp4;
+--		    data_temp6 <= data_temp5;
+--		    data_temp7 <= data_temp6;
 			FOR i IN taps-1 DOWNTO 0 LOOP
 			if i = 0 then 
-			 ADD(i) <= data_pipeline(i) * UNSIGNED(COEFS(i)) + ZERO ;
+			 ADD(i) <=  products(i) + ZERO ;
 			else
-			 ADD(i) <=  data_pipeline(i) * UNSIGNED(COEFS(i)) + ADD(i-1);
-			end if;  
-			END LOOP;
-			
-			result <= std_logic_vector(ADD(taps-1));	                           --output result
-			
+			 ADD(i) <=  products(i) + ADD(i-1);
+			end if; 
+			products(i) <= data_pipeline(i) * UNSIGNED(COEFS(i));
+			data_pipeline <= data_temp; 
+			data_temp <= UNSIGNED(data) & data_pipeline(0 TO taps-2);	--shift new data into data pipeline
+			END LOOP;			
 		END IF;
 	END PROCESS;
 	
